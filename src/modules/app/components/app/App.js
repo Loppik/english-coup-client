c423992573c3c852b31b4109617855bb1d25e571
Loppik <shantyr_qq@mail.ru>
2019-01-28 17:49:59 +0300

    added basic structure of main page

--- src/modules/app/components/app/App.js
+++ src/modules/app/components/app/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import MainPage from '../../../mainPage/components/mainPage/MainPage';
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-
+        <Route exact path="/" component={MainPage} />
       </React.Fragment>
     );
   }
