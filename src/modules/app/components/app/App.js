18e00a078b826b6a0f86613f153348756cbacedf
Loppik <shantyr_qq@mail.ru>
2019-02-05 19:36:27 +0300

    added registration form design

--- src/modules/app/components/app/App.js
+++ src/modules/app/components/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Login from '../../../login/components/login/Login';
+import Registration from '../../../registration/components/registration/Registration';
 import AddWordsPage from '../../../addWordsPage/components/addWordsPage/AddWordsPage';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/registration" component={Registration} />
         <Route exact path="/add" component={AddWordsPage} />
       </React.Fragment>
     );
