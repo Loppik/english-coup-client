008fb87960557daf6ea9111a185028aaaa38d276
Loppik <shantyr_qq@mail.ru>
2019-02-05 19:11:08 +0300

    added login form design

--- src/modules/app/components/app/App.js
+++ src/modules/app/components/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import Login from '../../../login/components/login/Login';
 import AddWordsPage from '../../../addWordsPage/components/addWordsPage/AddWordsPage';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Route exact path="/login" component={Login} />
         <Route exact path="/add" component={AddWordsPage} />
       </React.Fragment>
     );
---
+++ src/modules/login/components/login/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './login.css';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className={styles.content}>
+        <form>
+          <label>
+            <input type="text" required />
+            <div className={styles.labelText}>e-mail</div>
+          </label>
+          <label>
+            <input type="password" required />
+            <div className={styles.labelText}>password</div>
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login;
